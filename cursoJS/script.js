26d0f955a339db3441b19d233d2a0ad62be06484
Wander Massuci <wandermassuci@outlook.com>
2022-02-06 13:35:29 -0300

    commit all

---
+++ cursoJS/script.js
@@ -0,0 +1,7 @@
+let nome = prompt('Qual seu nome? ');
+let idade = parseInt(prompt('Qual sua idade? '));
+
+alert('Seu nome é ' +nome+ ' e voce tem ' +idade+ ' anos');
+
+//document.getElementById('nome').value = 'Oi'
+
